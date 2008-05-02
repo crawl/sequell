780b7615199d979ba28437a1422bc326fcaececb
Darshan Shaligram <scintilla@gmail.com>
2008-05-02 16:31:38 +0530

    Replace chars.py with chars.rb that queries the SQLite db.

---
+++ commands/chars.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/ruby
+
+require 'commands/sqlhelper'
+require 'commands/helper'
+
+help("Lists the frequency of all character types a player started.")
+
+who = ARGV[1]
+
+begin
+  chars = Hash.new(0)
+  q = build_query( who, -1, [ ] )
+  count = 0
+  name = who
+  sql_each_row_matching(  ) do |rgame|
+    game = row_to_fieldmap(rgame)
+    chars[game['char']] += 1
+    name = game['name']
+    count += 1
+  end
+
+  sorted =
+    chars.keys.map { |k| [k, chars[k]] }.sort { |a,b| b[1] <=> a[1] }
+
+  if sorted.empty?
+    puts "No games for #{name}."
+  else
+    puts("#{name} has played #{count} games: " +
+         sorted.map { |e| "#{e[1]}x#{e[0]}" }.join(" ")
+  end
+rescue
+  puts $!
+  raise
+end
