03270870194feca2d7be9aee77d27a5d694ef787
Darshan Shaligram <scintilla@gmail.com>
2008-07-19 16:28:52 +0530

    Added gkills replacement that queries the db.

---
+++ commands/gkills.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/ruby
+
+require 'commands/helper.rb'
+require 'commands/sqlhelper.rb'
+
+help("Lists the top kills for a player's ghost.")
+
+args = (ARGV[2].split)[1 .. -1]
+
+ghost = args[0]
+
+field = \
+  if ghost == '*'
+    "killer=~*'s ghost"
+  else
+    "killer=#{ghost}'s ghost"
+  end
+
+report_grouped_games('name', '', '*', [ '*', field ] + args[1 .. -1])
