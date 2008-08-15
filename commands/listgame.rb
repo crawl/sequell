eb173aa0721d63aee95a5a63b0d133a7ff1b3753
Henzell <henzell@crawl.(none)>
2008-08-15 07:41:21 -0400

    Update listgame help.

--- commands/listgame.rb
+++ commands/listgame.rb
@@ -3,6 +3,6 @@
 require 'commands/sqlhelper'
 require 'commands/helper'
 
-help("Lists a specifically-numbered game by a player with specified conditions. By default it lists the most recent game. Usage: !listgame (<player>) (<gamenumber>) (option list) where option list is in the form opt=value, or (max|min)=value. See ??listgame for more info.")
+help("Lists a specifically-numbered game by a player with specified conditions. By default it lists the most recent game. Usage: !listgame (<player>) (<gamenumber>) (options) where options are in the form field=value, or (max|min)=field. See ??listgame for more info.")
 
 sql_show_game(ARGV[1], ARGV[2].split()[1 .. -1])
