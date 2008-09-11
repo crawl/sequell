ba4dc23a734348b5f1a90981ed32c9096bca64ac
Darshan Shaligram <scintilla@gmail.com>
2008-09-11 14:27:42 +0530

    Add rune as an alias for urune.

--- commands/sqlhelper.rb
+++ commands/sqlhelper.rb
@@ -89,7 +89,7 @@ BOOLEAN_OR = '||'
 COLUMN_ALIASES = {
   'role' => 'cls', 'class' => 'cls', 'species' => 'race',
   'ktype' => 'ktyp', 'score' => 'sc', 'turns' => 'turn',
-  'time' => 'dur', 'skill' => 'sk'
+  'time' => 'dur', 'skill' => 'sk', 'rune' => 'urune'
 }
 
 LOGFIELDS_DECORATED = %w/file src v cv lv scI name uidI race crace cls
