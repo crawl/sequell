6b9acb68bb3c28ea5eb8ef75c92ed230d80f9fa6
Darshan Shaligram <scintilla@gmail.com>
2008-10-05 09:07:59 +0530

    Remove rune => urune mapping to unblock milestone query.

--- commands/sqlhelper.rb
+++ commands/sqlhelper.rb
@@ -95,7 +95,7 @@ BOOLEAN_OR_Q = Regexp.quote(BOOLEAN_OR)
 COLUMN_ALIASES = {
   'role' => 'cls', 'class' => 'cls', 'species' => 'race',
   'ktype' => 'ktyp', 'score' => 'sc', 'turns' => 'turn',
-  'time' => 'dur', 'skill' => 'sk', 'rune' => 'urune',
+  'time' => 'dur', 'skill' => 'sk',
   'ch' => 'char', 'r' => 'race', 'c' => 'cls', 'sp' => 'race',
   'cl' => 'xl', 'clev' => 'xl', 'type' => 'verb'
 }
