a885e8d59574b0460a992c89e10039d2028beb98
Darshan Shaligram <scintilla@gmail.com>
2008-10-14 18:21:53 +0530

    Simplify killsby, add deathsby that queries MySQL instead of the old logfile.

---
+++ commands/deathsby.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/ruby
+
+require 'commands/helper.rb'
+require 'commands/sqlhelper.rb'
+
+help("Lists the most frequent causes of death for a player.")
+
+args = ARGV[2].split()[1 .. -1]
+
+report_grouped_games('ckiller', '', ARGV[1], args)
--- commands/killsby.rb
+++ commands/killsby.rb
@@ -2,31 +2,11 @@
 
 require 'commands/helper.rb'
 require 'commands/sqlhelper.rb'
-require 'set'
 
-help("Lists the top 10 player kills for a given monster.")
-
-KTYP_FIELDS = [
-               'pois', 'starvation', 'stupidity', 'water', 'burning',
-               'draining', 'weakness', 'cloud', 'lava', 'clumsiness',
-               'trap', 'freezing', 'wild_magic', 'statue', 'rotting',
-               'targeting', 'spore', 'falling_down_stairs', 'petrification',
-               'acid', 'curare', 'melting', 'bleeding', 'statue', 'xom',
-               'tso_smiting', 'deaths_door'
-               ]
-KTYP_MAPPINGS = {
-  "drown" => "water", "drowning" => 'water',
-  'poison' => 'pois', 'poisoning' => 'pois'
-}
-KTYP_SET = Set.new(KTYP_FIELDS)
+help("Lists the most frequent victims for a given monster.")
 
 def killer_field(key)
-  key = KTYP_MAPPINGS[key.downcase] || key
-  if KTYP_SET.include?(key.downcase)
-    "ktyp=#{key}"
-  else
-    "killer=#{key}"
-  end
+  "ckiller=#{key}"
 end
 
 args = sanitize_args( (ARGV[2].split)[1 .. -1] )
