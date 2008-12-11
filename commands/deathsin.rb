635c6abd4ba9447346803f2e8c6b19dea5ea9d31
Darshan Shaligram <scintilla@gmail.com>
2008-12-11 12:35:07 +0530

    Fix deathsin not using group query support.

--- commands/deathsin.rb
+++ commands/deathsin.rb
@@ -3,60 +3,16 @@
 require 'commands/sqlhelper'
 require 'commands/helper'
 
-MAX_LIST = 10
+help("Lists the players who've died most frequently in a certain place.")
 
-help("Lists the top #{MAX_LIST} players to die in a certain place.")
+args = sanitize_args((ARGV[2].split)[1 .. -1])
 
-args = ARGV[2].split
-place = args[1]
-rest = args[2 .. -1]
+params = [ "*",
+           "place=#{args[0]}",
+           "ktyp!=quitting",
+           "ktyp!=leaving",
+           "ktyp!=winning" ]
 
-def death_report(count, victims)
-  vlist = victims.keys.map { |k| [ k, victims[k] ] }.sort { |b, a| a[1] <=> b[1] }
-  vlist = vlist[0 ... MAX_LIST]
-  freq = ""
-  vlist.each do |name, times|
-    freq << "#{times}x #{name}(#{sprintf('%.2f%%', times * 100.0 / count)}) "
-  end
-  freq
-end
+args = args[1 .. -1]
 
-begin
-  q = build_query('*', -1, [ "place=#{place}",
-                             "ktyp!=quitting",
-                             "ktyp!=winning",
-                             "ktyp!=leaving" ] + paren_args(rest))
-
-  count = sql_count_rows_matching(q)
-  if count > 2000
-    raise "Too many matching games (#{count})."
-  end
-
-  victims = Hash.new(0)
-  canonplace = nil
-  canonbranch = nil
-  sql_each_row_matching(q) do |row|
-    map = row_to_fieldmap(row)
-    victims[map['name']] += 1
-
-    thisplace = map['place']
-    canonplace = thisplace unless canonplace
-
-    if canonplace != thisplace
-      canonbranch = map['ltyp'] != 'D' ? map['ltyp'] : map['br']
-    end
-  end
-
-  place = canonbranch || canonplace || place
-
-  if count == 0
-    puts "No deaths in #{place}."
-  elsif count == 1
-    puts "One death in #{place}. Most frequent: #{death_report(count, victims)}"
-  else
-    puts "#{count} deaths in #{place}. Most frequent: #{death_report(count, victims)}"
-  end
-rescue
-  puts $!
-  raise
-end
+report_grouped_games('name', '', '*', params + paren_args(args))
