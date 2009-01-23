2bc0e3b6cbeb5e265bf51eec3d34478c6c07af0e
Darshan Shaligram <scintilla@gmail.com>
2009-01-24 00:08:09 +0530

    Add script to update unwon combos list.

---
+++ unwon.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW distinct_combos
+AS SELECT DISTINCT charabbrev FROM logrecord;
+
+CREATE OR REPLACE VIEW unwon_combos
+AS SELECT dc.charabbrev FROM distinct_combos dc
+WHERE NOT EXISTS (SELECT * FROM logrecord lr WHERE
+                  lr.charabbrev = dc.charabbrev
+                  AND ktyp = 'winning');
+
+SELECT lr.charabbrev combo, COUNT(*) games
+FROM logrecord lr, unwon_combos uw
+WHERE lr.charabbrev = uw.charabbrev
+GROUP BY lr.charabbrev
+ORDER BY games DESC;
