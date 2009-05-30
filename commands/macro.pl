5cb2f8e4257115ceede5701c08996dd76208b468
rax <rax@crawl.akrasiac.org>
2009-05-30 12:17:49 -0400

    macro.pl fix.

--- commands/macro.pl
+++ commands/macro.pl
@@ -7,10 +7,10 @@ my $nick = shift;
 my $baseURL = "http://crawl.akrasiac.org/rcfiles/";
 my $localPath = "/var/www/crawl/rcfiles/";
 
-my $localRC = $localPath . $nick . ".nethackrc";
+my $localRC = $localPath . $nick . ".crawlrc";
 
 do 'commands/helper.pl';
 help("Gives an URL to the specified users crawl configuration file.");
 
-if(-e $localRC)	{ print $baseURL . $nick . ".macro\n"; }
+if(-e $localRC)	{ print $baseURL . $nick . ".macro.04\n"; }
 else			{ print "User does not exist.\n"; }
