514f9598939c8226c6d2126db8b0782d516afbea
Darshan Shaligram <scintilla@gmail.com>
2009-07-13 19:50:39 +0530

    Allow using race and class abbreviations without an explit x=

--- commands/sqlhelper.rb
+++ commands/sqlhelper.rb
@@ -920,6 +920,9 @@ def fixup_listgame_param(arg)
   if arg =~ /^([a-z]{2})([a-z]{2})/i && RACE_EXPANSIONS[$1.downcase] \
     && CLASS_EXPANSIONS[$2.downcase] then
     return "char=" + arg
+  elsif arg =~ /^[a-z]{2}$/ then
+    return "cls=" + arg if CLASS_EXPANSIONS[arg] && !RACE_EXPANSIONS[arg]
+    return "race=" + arg if RACE_EXPANSIONS[arg] && !CLASS_EXPANSIONS[arg]
   end
   nil
 end
