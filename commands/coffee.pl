71824fa105fec0065820e6763412dd8f8d09b1b4
Darshan Shaligram <scintilla@gmail.com>
2010-02-22 17:27:37 +0530

    Experimental !coffee command.

---
+++ commands/coffee.pl
@@ -0,0 +1,70 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib 'commands';
+use Helper;
+use utf8;
+
+binmode STDOUT, ':utf8';
+help("Brews a delicious mug of coffee!");
+
+my $nick = shift;
+
+my @coffee_basetypes =
+  ("black coffee",
+   "cappuccino",
+   "irish coffee",
+   "café au lait"
+  );
+
+my @brewers =
+  (
+   'Sigmund',
+   'Ijyb',
+   'Cerebov',
+   'Crazy Yiuf',
+   'Snorg',
+   'the Serpent of Hell',
+   'Nikola'
+  );
+
+my @containers =
+  (
+   'cup',
+   'mug',
+   'pot',
+   { wt => 20, name => 'barrel' },
+   { wt => 1, name => 'supertanker' }
+  );
+
+my $coffee = do_coffee();
+print "/me hands $nick $coffee.";
+
+sub random_from {
+  my $chosen;
+  my $weight = 0;
+  for my $thing (@_) {
+    my $thisweight = 100;
+    my $this = $thing;
+    if (ref($thing) eq 'HASH') {
+      $thisweight = $thing->{wt};
+      $this = $thing->{name};
+    }
+    $chosen = $this if int(rand($weight += $thisweight)) < $thisweight;
+  }
+  return $chosen;
+}
+
+sub do_coffee {
+  my $coffee_kind = random_from(@coffee_basetypes);
+  my $brewer = random_from(@brewers);
+  my $need_container = $coffee_kind !~ /^an? /;
+  if ($need_container) {
+    my $container = random_from(@containers);
+    return "a $container of $coffee_kind, brewed by $brewer";
+  } else {
+    return "$coffee_kind, brewed by $brewer";
+  }
+}
