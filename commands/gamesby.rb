5a3ac7e82f045093f0823acb491eb3a6c05c4341
Darshan Shaligram <scintilla@gmail.com>
2010-07-20 05:14:44 +0530

    Update gamesby for Un.

--- commands/gamesby.rb
+++ commands/gamesby.rb
@@ -8,9 +8,9 @@ help("Summarizes a player's crawl.akrasiac.org career.")
 q = sql_build_query(ARGV[1], (ARGV[2].split)[1 .. -1])
 
 full_query = \
-q.select("MAX(#{LOG2SQL['name']}), COUNT(*), MIN(#{LOG2SQL['start']}), " +
-         "MAX(#{LOG2SQL['end']}), " +
-         "MAX(sc), SUM(sc), SUM(turn), SUM(dur)", false)
+q.select("MAX(#{LOG2SQL['name']}), COUNT(*), MIN(#{LOG2SQL['starttime']}), " +
+         "MAX(#{LOG2SQL['endtime']}), " +
+         "MAX(points), SUM(points), SUM(turns), SUM(realtime)", false)
 
 rows = []
 sql_each_row_for_query(full_query, *q.values) do |r|
@@ -40,12 +40,12 @@ else
     sql_count_rows_matching(
        sql_build_query(ARGV[1],
                        paren_args((ARGV[2].split)[1 .. -1]) +
-                       ["ktyp=winning"]))
+                       ["ktype=ascended"]))
 
   tstart = sql2logdate(r[2])
   tend = sql2logdate(r[3])
   puts "#{q.argstr} has played #{ngames} game#{plural}, between " +
-      "#{datestr(tstart)} and #{datestr(tend)}, won #{winstr(win_count, ngames)}, " +
+      "#{datestr(tstart)} and #{datestr(tend)}, ascended #{winstr(win_count, ngames)}, " +
       "high score #{r[4]}, total score #{sqlnumber(r[5])}, total turns #{sqlnumber(r[6])}, " +
       "total time #{duration_str(r[7].to_i)}."
 end
