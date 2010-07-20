6ba6a428555b3afb85ba2e524c8ebebd63b3a4be
Darshan Shaligram <scintilla@gmail.com>
2010-07-20 20:10:04 +0530

    Switch commands to =foo to avoid collisions with existing bots.

--- commands/help.pl
+++ commands/help.pl
@@ -8,14 +8,15 @@ Henzell::Config::read();
 
 # prep the new @ARGV for the help command
 $ARGV[3] = 1;
-$ARGV[2] =~ s/^!help\s+//i;
-$ARGV[2] = "!$ARGV[2]" unless !$ARGV[2] || substr($ARGV[2], 0, 1) eq '!';
+my ($key) = $ARGV[2] =~ /^(.)help/i;
+
+$ARGV[2] =~ s/^.help\s+//i;
+$ARGV[2] = "$key$ARGV[2]" unless !$ARGV[2] || substr($ARGV[2], 0, 1) eq $key;
 
 # which command are we going for?
 $ARGV[2] =~ /(\S+)/;
-my $requested = lc(defined($1) ? $1 : "!help");
+my $requested = lc(defined($1) ? $1 : "${key}help");
 
 if ($CMDPATH{$requested}) {
-  print "$requested: ";
   exec $CMDPATH{$requested}, @ARGV;
 }
