5df6ffbc0575556fd1e0ed56a2fc923973597703
Darshan Shaligram <scintilla@gmail.com>
2010-07-26 01:45:30 +0530

    Add Yiuf to the hitlist.

--- henzell.pl
+++ henzell.pl
@@ -61,7 +61,7 @@ my $DEV_CHANNEL      = $CONFIG{dev_channel};
 my @BORING_UNIQUES = qw/Jessica Ijyb Blork Terence Edmund Psyche
                         Joseph Josephine Harold Norbert Jozef
                         Maud Duane Grum Gastronok Dowan Duvessa
-                        Pikel Menkaure Purgy Grinder Maurice/;
+                        Pikel Menkaure Purgy Grinder Maurice Yiuf/;
 
 binmode STDOUT, ':utf8';
 
