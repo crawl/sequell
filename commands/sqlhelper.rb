e3d94be98d616a1faecb09246c2324a7d8ecd2b4
Darshan Shaligram <scintilla@gmail.com>
2010-08-28 03:32:11 +0530

    Accept ? wildcards for globs.

--- commands/sqlhelper.rb
+++ commands/sqlhelper.rb
@@ -1906,7 +1906,7 @@ end
 
 def proc_val(val, sqlop)
   if sqlop =~ /LIKE/
-    val = val.index('*') ? val.tr('*', '%') : "%#{val}%"
+    val = val.index('*') or val.index('?') ? val.tr('*?', '%_') : "%#{val}%"
   end
   val
 end
