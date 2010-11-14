efe91d3fe05c0c69663cabfa9197b48f1c492b47
Darshan Shaligram <scintilla@gmail.com>
2010-11-14 19:04:16 +0530

    Preliminary work on treetop parser for !lg queries.

---
+++ commands/lgparse.rb
@@ -0,0 +1,59 @@
+require 'rubygems'
+require 'treetop'
+require 'lg'
+
+parser = ListgameQueryParser.new
+
+def node_name(node)
+  node.to_s.sub(/.*:/, '')
+end
+
+def not_empty?(x)
+  x and not x.strip.empty?
+end
+
+def indent(level)
+  dent = ""
+  (level - 1).times do
+    dent << "    "
+  end
+  dent << "|---#{level}-" if level > 0
+  dent
+end
+
+def string_tree(node, level=0)
+  return "ERR" if node.nil?
+
+  em = node.extension_modules
+  elements = node.elements
+  if em and not em.empty?
+    base = indent(level) + "#{node_name(em)} \"#{node.text_value.strip}\""
+    if elements and not elements.empty?
+      estr = elements.map { |e| string_tree(e, level + 1) }.find_all { |x| not_empty?(x) }.join("\n")
+      if not estr.empty?
+        return base + "\n" + estr
+      end
+    end
+    return base
+  end
+  if not elements or elements.empty?
+    nil
+  else
+    text = elements.map { |e| string_tree(e, level) }.find_all { |x| not_empty?(x) }.join("\n")
+    text
+  end
+end
+
+while true
+  print "Enter text to parse: "
+  x = gets
+  break unless x
+  x.strip!
+  tree = parser.parse(x)
+  if tree.nil?
+    STDERR.puts("Parser error at:\n#{x}\n" +
+                sprintf("%*s", parser.index - 1, "") + "^")
+  else
+    puts "Parsed text: '#{x}' to:\n#{string_tree(tree)})"
+  end
+end
