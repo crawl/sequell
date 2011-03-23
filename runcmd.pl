58e694c75aa56d7e7461dceeef04fbe6221885c1
Darshan Shaligram <scintilla@gmail.com>
2011-03-23 12:11:37 +0530

    Make runcmd accept the command to run on the command line.

--- runcmd.pl
+++ runcmd.pl
@@ -5,10 +5,21 @@ use warnings;
 
 use Henzell::Cmd qw/load_all_commands execute_cmd/;
 
-print "Henzell command runner\n";
 Henzell::Cmd::load_all_commands();
-while ( my $cmd = do { print "> "; <STDIN> } ) {
-  chomp($cmd);
+
+sub runcmd($) {
+  chomp(my $cmd = shift);
   my ($exit, $result) = Henzell::Cmd::execute_cmd('nobody', $cmd);
+  chomp $result;
   print "$result\n";
+}
+
+if (@ARGV > 0) {
+  runcmd(join(' ', @ARGV));
+  exit 0;
+}
+
+print "Henzell command runner\n";
+while ( my $cmd = do { print "> "; <STDIN> } ) {
+  runcmd($cmd);
 }
