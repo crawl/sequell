aeb6132b1ceee9117a461129e6a82c9db4e004bd
Samuel Bronson <naesten@gmail.com>
2011-10-24 16:28:57 -0400

    Improve help for listgame command.

--- commands/listgame.rb
+++ commands/listgame.rb
@@ -4,6 +4,6 @@ require 'commands/sqlhelper'
 require 'commands/helper'
 require 'commands/libtv'
 
-help("Lists a specifically-numbered game by a player with specified conditions. By default it lists the most recent game. Usage: !listgame (<player>) (<gamenumber>) (options) where options are in the form field=value, or (max|min)=field. See ??listgame for more info.")
+help("Lists games matching specified conditions. By default it lists the most recent game played by the invoker. Usage: !listgame (<player>) (<gamenumber>) (options) where options are in the form field=value, or (max|min)=field. See ??listgame or http://is.gd/sequell_lg for more info.")
 
 sql_show_game_with_extras(ARGV[1], ARGV[2])
