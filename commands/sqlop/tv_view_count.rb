dd46613a323fcc6bd33a6d26a6bf1e5c34850aea
Darshan Shaligram <scintilla@gmail.com>
2012-10-29 14:59:56 -0400

    Let TVViewCount handle null ntv correctly.

--- commands/sqlop/tv_view_count.rb
+++ commands/sqlop/tv_view_count.rb
@@ -2,8 +2,8 @@ module Sqlop
   class TVViewCount
     def self.increment(game)
       table = game['sql_table'] or raise "No sql_table field in game"
-      sql_db_handle.do("UPDATE #{table} SET ntv = ntv + 1 " +
-        "WHERE id = ?", game['id'])
+      sql_db_handle.do("UPDATE #{table} SET ntv = NVL(ntv, 0) + 1 " +
+                       "WHERE id = ?", game['id'])
     end
   end
 end
