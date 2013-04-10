6c16d8ce141e7b88289ebf3064c04ea30f30ab76
Darshan Shaligram <scintilla@gmail.com>
2013-04-10 18:14:37 +0530

    Fix echo.

--- commands/echo.pl
+++ commands/echo.pl
@@ -9,4 +9,7 @@ use Helper;
 binmode STDIN, ':utf8';
 binmode STDOUT, ':utf8';
 help("Echoes the command to the channel");
-print "$ARGV[2]\n";
+
+my $cmd = $ARGV[2];
+$cmd =~ s/^\S+ //;
+print "$cmd\n";
