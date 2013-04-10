a17a9708ec54971c2f535a3a65ac28ca88dc92ba
Darshan Shaligram <scintilla@gmail.com>
2013-04-10 17:30:16 +0530

    Correctly handle positional args again.

--- src/query/query_string_template.rb
+++ src/query/query_string_template.rb
@@ -1,27 +1,27 @@
 module Query
-  class QueryStringTemplate
-    def self.substitute(query_string, argument_lists)
-      argument_lists.reduce(query_string.to_s) { |query, arglist|
-        substitute_query_args(query, arglist.strip)
-      }
-    end
+   class QueryStringTemplate
+     def self.substitute(query_string, argument_lists)
+       argument_lists.reduce(query_string.to_s) { |query, arglist|
+         substitute_query_args(query, arglist.strip)
+       }
+     end
 
-    def self.substitute_query_args(query_string, arglist)
-      require 'tpl/template'
+     def self.substitute_query_args(query_string, arglist)
+       require 'tpl/template'
 
-      args = arglist.split(' ')
-      rest_args_used = false
-      max_index = 0
+       args = arglist.split(' ')
+       rest_args_used = false
+       max_index = 0
 
-      arg_provider = lambda { |key|
-        return nil unless key =~ /^\d+$/ || key == '*'
+       arg_provider = lambda { |key|
+         return nil unless key =~ /^\d+$/ || key == '*'
         if key == '*'
           rest_args_used = true
           (args[max_index..-1] || []).join(' ')
         else
           index = key.to_i
           max_index = index if index > max_index
-          args[index - 1]
+          args[index - 1] || ''
         end
       }
       res = Tpl::Template.template_eval(query_string, arg_provider)
