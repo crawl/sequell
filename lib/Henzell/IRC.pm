b587384b165537810cf98aa049460b91cee80d55
Darshan Shaligram <scintilla@gmail.com>
2013-11-26 20:44:21 -0500

    Reduce debug spam.

--- lib/Henzell/IRC.pm
+++ lib/Henzell/IRC.pm
@@ -34,35 +34,35 @@ sub connected {
 
 sub emoted {
   my ($self, $e) = @_;
-  print "emoted: ", Dumper($e), "\n";
+  print "emoted: ", Dumper($e), "\n" if $ENV{DEBUG_HENZELL};
   $self->_each_service_call('event_emoted', $self->_message_metadata($e));
   return undef;
 }
 
 sub chanjoin {
   my ($self, $j) = @_;
-  print "chanjoin: ", Dumper($j), "\n";
+  print "chanjoin: ", Dumper($j), "\n" if $ENV{DEBUG_HENZELL};
   $self->_each_service_call('event_chanjoin', $self->_message_metadata($j));
   return undef;
 }
 
 sub userquit {
   my ($self, $q) = @_;
-  print "quit: ", Dumper($q), "\n";
+  print "quit: ", Dumper($q), "\n" if $ENV{DEBUG_HENZELL};
   $self->_each_service_call('event_userquit', $self->_message_metadata($q));
   return undef;
 }
 
 sub chanpart {
   my ($self, $m) = @_;
-  print "part: ", Dumper($m), "\n";
+  print "part: ", Dumper($m), "\n" if $ENV{DEBUG_HENZELL};
   $self->_each_service_call('event_chanpart', $self->_message_metadata($m));
   return undef;
 }
 
 sub said {
   my ($self, $m) = @_;
-  print "said: ", Dumper($m), "\n";
+  print "said: ", Dumper($m), "\n" if $ENV{DEBUG_HENZELL};
   $self->_each_service_call('event_said', $self->_message_metadata($m));
   return undef;
 }
