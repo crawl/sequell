006b23e035c9c0dd695012216981913347287f35
Darshan Shaligram <scintilla@gmail.com>
2013-11-30 10:06:20 -0500

    Apply timeout to Sequell LearnDB expansions.

--- commands/echo-pipe.rb
+++ commands/echo-pipe.rb
@@ -4,23 +4,30 @@ require 'query/query_string_template'
 require 'json'
 require 'env'
 require 'command_context'
+require 'timeout'
 
 STDIN.sync = true
 STDERR.sync = true
 STDOUT.sync = true
 
+TIME_LIMIT = 60
+
 begin
   while (line = STDIN.readline)
     begin
       data = JSON.parse(line)
-      Env.with(data['command_env']) do
-        CommandContext.with_default_nick((data['env'] || { })['nick']) do
-          puts JSON.dump(res: Query::QueryStringTemplate.expand(
-              data['msg'],
-              data['args'] || '',
-              data['env'] || { }))
+      Timeout.timeout(TIME_LIMIT) {
+        Env.with(data['command_env']) do
+          CommandContext.with_default_nick((data['env'] || { })['nick']) do
+            puts JSON.dump(res: Query::QueryStringTemplate.expand(
+                data['msg'],
+                data['args'] || '',
+                data['env'] || { }))
+          end
         end
-      end
+      }
+    rescue Timeout::Error
+      puts JSON.dump(err: "Time limit of #{TIME_LIMIT} exceeded")
     rescue
       puts JSON.dump(err: $!.message)
     end
