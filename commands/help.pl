379a772c2ffcbe87367714b88096d99151511c15
Darshan Shaligram <scintilla@gmail.com>
2013-12-08 14:34:37 -0500

    Make !help respect sigils.

--- commands/help.pl
+++ commands/help.pl
@@ -9,10 +9,12 @@ Helper::help("Displays help on a command. For a list of commands, see !cmdinfo."
 
 Henzell::Config::read();
 
+my $sigils = Henzell::Config::sigils();
+
 # prep the new @ARGV for the help command
 $ARGV[3] = 1;
 $ARGV[2] =~ s/^!help\s+//i;
-$ARGV[2] = "!$ARGV[2]" unless !$ARGV[2] || substr($ARGV[2], 0, 1) eq '!';
+$ARGV[2] = "!$ARGV[2]" unless !$ARGV[2] || substr($ARGV[2], 0, 1) =~ /[\Q$sigils\E]/;
 
 # which command are we going for?
 $ARGV[2] =~ /(\S+)/;
