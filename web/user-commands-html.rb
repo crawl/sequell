6202c58d3619c6f959d6c4cdcb5dd1aeae138bf2
Darshan Shaligram <scintilla@gmail.com>
2013-12-09 19:57:40 -0500

    Handle sqlite busy errors.

--- web/user-commands-html.rb
+++ web/user-commands-html.rb
@@ -9,23 +9,34 @@ require 'cgi'
 
 TEMPLATE = 'web/user-defined.html.haml'
 
-db = Cmd::UserCommandDb.db
-keywords = db.keywords
-commands = db.commands
-functions = db.functions
-behaviours = LearnDB::DB.default.entry(':beh:').definitions
-
 def h(text)
   CGI.escapeHTML(text)
 end
 
-puts Haml::Engine.new(File.read(TEMPLATE)).render(
-  Object.new,
-  keywords: keywords,
-  commands: commands,
-  functions: functions,
-  behaviours: behaviours.map { |x|
-    res = x.split(':::')
-    res.unshift(nil) if res.size == 1
-    res
-  })
+db = Cmd::UserCommandDb.db
+
+tries = 10
+while tries > 0
+  begin
+    keywords = db.keywords
+    commands = db.commands
+    functions = db.functions
+    behaviours = LearnDB::DB.default.entry(':beh:').definitions
+
+    puts Haml::Engine.new(File.read(TEMPLATE)).render(
+      Object.new,
+      keywords: keywords,
+      commands: commands,
+      functions: functions,
+      behaviours: behaviours.map { |x|
+        res = x.split(':::')
+        res.unshift(nil) if res.size == 1
+        res
+      })
+
+    break
+  rescue SQLite3::BusyException
+    sleep 1
+    tries -= 1
+  end
+end
