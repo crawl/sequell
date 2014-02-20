6d984cb42753cb1efef812f7d6449f7111761c17
Darshan Shaligram <scintilla@gmail.com>
2014-02-20 09:45:13 +0530

    Toss redundant commands.

--- commands/hsn.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'helper'
-require 'sqlhelper'
-require 'libtv'
-
-help "Lists the highest-scoring game for a player."
-
-sql_show_game_with_extras(ARGV[1], ARGV[2], [ "max=sc" ])
