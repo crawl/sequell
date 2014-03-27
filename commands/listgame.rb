b675d7b30b59624a6fad68874dae118053b7825b
Darshan Shaligram <scintilla@gmail.com>
2014-03-26 23:17:21 -0400

    Remove hardcoded URL from !lg help.

--- commands/listgame.rb
+++ commands/listgame.rb
@@ -5,6 +5,6 @@ require 'sqlhelper'
 require 'helper'
 require 'libtv'
 
-help("Lists games matching specified conditions. By default it lists the most recent game played by the invoker. Usage: !listgame (<player>) (<gamenumber>) (options) where options are in the form field=value, or (max|min)=field. See ??listgame or http://is.gd/sequell_lg for more info.")
+help("Lists games matching specified conditions. By default it lists the most recent game played by the invoker. Usage: !listgame (<player>) (<gamenumber>) (options) where options are in the form field=value, or (max|min)=field. See ??listgame for more info.")
 
 sql_show_game_with_extras(ARGV[1], ARGV[2])
