e50786ef6c1850a236f555966427b6570bc5184c
Darshan Shaligram <scintilla@gmail.com>
2014-10-15 15:14:29 -0400

    Use https for graph URLs.

--- src/graph/url.rb
+++ src/graph/url.rb
@@ -2,7 +2,7 @@ module Graph
   class Url
     def self.file_url(filename)
       require 'henzell/env'
-      "http://#{Henzell::Env.hostname}/graphs/#{filename}"
+      "https://#{Henzell::Env.hostname}/graphs/#{filename}"
     end
   end
 end
